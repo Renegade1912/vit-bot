b7faaef22dac72edda0eb66d8826a798bbceff31
Hyperdomi <67032161+Hyperdomi@users.noreply.github.com>
2023-11-30 11:21:36 +0100

    Dijkstra

---
+++ src/de/vit/models/Dijkstra.java
@@ -0,0 +1,11 @@
+package de.vit.models;
+
+public class Dijkstra {
+
+}
+// TODO
+// - multidimensionales Array, Cell werten int werte zuordnen
+// - Leerstellen aufüllen
+// - Dijkstra Berechnung
+// - Laufentscheidung zurückgeben
+// - übergabe parameter currentx,y
